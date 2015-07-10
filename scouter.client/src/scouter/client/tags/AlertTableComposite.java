827e3e5799494b780e2aa96abe8e6e78099b1cf9
ScouterProject <scouter.project@gmail.com>
2015-07-10 17:15:36 +0900

    tagcnt update

---
+++ scouter.client/src/scouter/client/tags/AlertTableComposite.java
@@ -0,0 +1,216 @@
+/*
+ *  Copyright 2015 LG CNS.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); 
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License. 
+ */
+package scouter.client.tags;
+
+import java.util.ArrayList;
+
+import org.eclipse.jface.layout.TableColumnLayout;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ColumnPixelData;
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.IDoubleClickListener;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+
+import scouter.client.model.TextProxy;
+import scouter.client.popup.AlertNotifierDialog;
+import scouter.client.sorter.TableLabelSorter;
+import scouter.lang.AlertLevel;
+import scouter.lang.pack.AlertPack;
+import scouter.lang.pack.Pack;
+import scouter.util.DateUtil;
+
+public class AlertTableComposite extends Composite {
+
+	Composite parent;
+
+	private TableViewer viewer;
+	private TableColumnLayout tableColumnLayout;
+
+	int serverId;
+	String yyyymmdd;
+
+	public AlertTableComposite(Composite parent, int style) {
+		super(parent, style);
+		this.parent = this;
+		initLayout();
+	}
+	
+	private void initLayout() {
+		parent.setLayout(new FillLayout());
+		Composite comp = new Composite(parent, SWT.NONE);
+		tableColumnLayout = new TableColumnLayout();
+		comp.setLayout(tableColumnLayout);
+		viewer = new TableViewer(comp, SWT.FULL_SELECTION | SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL);
+		createColumns();
+		final Table table = viewer.getTable();
+	    table.setHeaderVisible(true);
+	    table.setLinesVisible(true);
+	    viewer.setContentProvider(new ArrayContentProvider());
+	    viewer.setComparator(new TableLabelSorter(viewer));
+	    viewer.addDoubleClickListener(new IDoubleClickListener() {
+			public void doubleClick(DoubleClickEvent e) {
+				StructuredSelection sel = (StructuredSelection) viewer.getSelection();
+				Object o = sel.getFirstElement();
+				if (o instanceof AlertPack) {
+					AlertNotifierDialog alertDialog = new AlertNotifierDialog(parent.getDisplay(), serverId, (AlertPack)o);
+					alertDialog.show(parent.getBounds());
+				} else {
+					System.out.println(o);
+				}
+			}
+		});
+	}
+	
+	public void setInput(ArrayList<Pack> packList, int serverId, String date) {
+		this.serverId = serverId;
+		this.yyyymmdd = date;
+		viewer.setInput(packList);
+	}
+	
+	ArrayList<AlertColumnEnum> columnList = new ArrayList<AlertColumnEnum>();
+	
+	private void createColumns() {
+		for (AlertColumnEnum column : AlertColumnEnum.values()) {
+			createTableViewerColumn(column.getTitle(), column.getWidth(), column.getAlignment(), column.isResizable(), column.isMoveable(), column.isNumber());
+			columnList.add(column);
+		}
+		viewer.setLabelProvider(new TableItemProvider());
+	}
+	
+	class TableItemProvider implements ITableLabelProvider {
+
+		public void addListener(ILabelProviderListener listener) {
+			
+		}
+
+		public void dispose() {
+			
+		}
+
+		public boolean isLabelProperty(Object element, String property) {
+			return false;
+		}
+
+		public void removeListener(ILabelProviderListener listener) {
+		}
+
+		public Image getColumnImage(Object element, int columnIndex) {
+			return null;
+		}
+
+		public String getColumnText(Object element, int columnIndex) {
+			if (element instanceof AlertPack == false) {
+				return null;
+			} 
+			AlertPack p = (AlertPack) element;
+			AlertColumnEnum column = columnList.get(columnIndex);
+			switch (column) {
+			case TIME :
+				return DateUtil.format(p.time, "HH:mm:ss.sss");
+			case LEVEL :
+				return AlertLevel.getName(p.level);
+			case OBJECT :
+				return TextProxy.object.getLoadText(yyyymmdd, p.objHash, serverId);
+			case TITLE :
+				return p.title;
+			case MESSAGE :
+				return p.message;
+			case TAGS :
+				return p.tags.toString();
+			}
+			return null;
+		}
+	}
+	
+	private TableViewerColumn createTableViewerColumn(String title, int width, int alignment,  boolean resizable, boolean moveable, final boolean isNumber) {
+		final TableViewerColumn viewerColumn = new TableViewerColumn(viewer, SWT.NONE);
+		final TableColumn column = viewerColumn.getColumn();
+		column.setText(title);
+		column.setAlignment(alignment);
+		column.setMoveable(moveable);
+		tableColumnLayout.setColumnData(column, new ColumnPixelData(width, resizable));
+		column.setData("isNumber", isNumber);
+		column.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				TableLabelSorter sorter = (TableLabelSorter) viewer.getComparator();
+				TableColumn selectedColumn = (TableColumn) e.widget;
+				sorter.setColumn(selectedColumn);
+			}
+		});
+		return viewerColumn;
+	}
+	
+	enum AlertColumnEnum {
+	    TIME("TIME", 100, SWT.RIGHT, true, true, true), //
+	    LEVEL("LEVEL", 70, SWT.CENTER, true, true, false), //
+	    OBJECT("OBJECT", 100, SWT.LEFT, true, true, false),
+	    TITLE("TITLE", 150, SWT.LEFT, true, true, false),
+	    MESSAGE("MESSAGE", 250, SWT.LEFT, true, true, false),
+	    TAGS("TAGs", 200, SWT.LEFT, true, true, false);
+
+	    private final String title;
+	    private final int weight;
+	    private final int alignment;
+	    private final boolean resizable;
+	    private final boolean moveable;
+	    private final boolean isNumber;
+
+	    private AlertColumnEnum(String text, int weight, int alignment, boolean resizable, boolean moveable, boolean isNumber) {
+	        this.title = text;
+	        this.weight = weight;
+	        this.alignment = alignment;
+	        this.resizable = resizable;
+	        this.moveable = moveable;
+	        this.isNumber = isNumber;
+	    }
+	    
+	    public String getTitle(){
+	        return title;
+	    }
+
+	    public int getAlignment(){
+	        return alignment;
+	    }
+
+	    public boolean isResizable(){
+	        return resizable;
+	    }
+
+	    public boolean isMoveable(){
+	        return moveable;
+	    }
+
+		public int getWidth() {
+			return weight;
+		}
+		
+		public boolean isNumber() {
+			return this.isNumber;
+		}
+	}
+}
--- scouter.client/src/scouter/client/tags/TagCountView.java
+++ scouter.client/src/scouter/client/tags/TagCountView.java
@@ -59,6 +59,8 @@ import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.FillLayout;
@@ -91,11 +93,14 @@ import scouter.client.util.ExUtil;
 import scouter.client.util.ImageUtil;
 import scouter.client.util.ScouterUtil;
 import scouter.io.DataInputX;
+import scouter.lang.pack.AlertPack;
 import scouter.lang.pack.MapPack;
 import scouter.lang.pack.Pack;
 import scouter.lang.pack.XLogPack;
 import scouter.lang.value.BooleanValue;
 import scouter.lang.value.DecimalValue;
+import scouter.lang.value.ListValue;
+import scouter.lang.value.MapValue;
 import scouter.lang.value.NullValue;
 import scouter.lang.value.Value;
 import scouter.net.RequestCmd;
@@ -127,6 +132,7 @@ public class TagCountView extends ViewPart {
 	XYGraph totalGraph;
 	Trace totalTrace;
 	
+	Label rangeLbl;
 	Label dataRangeLbl;
 	Button leftBtn;
 	Button rightBtn;
@@ -147,6 +153,7 @@ public class TagCountView extends ViewPart {
 	
 	SashForm dataTableSash;
 	ServiceTableComposite serviceTable;
+	AlertTableComposite alertTable;
 	
 	int lastWidth = 1;
 	
@@ -260,7 +267,7 @@ public class TagCountView extends ViewPart {
 				if (stime < rangeX1 || stime > rangeX2) return;
 				stime = stime / DateUtil.MILLIS_PER_MINUTE * DateUtil.MILLIS_PER_MINUTE;
 				long etime = stime + DateUtil.MILLIS_PER_MINUTE - 1;
-				loadInitData(tagGroupCombo.getText(), stime, etime);
+				loadInitData(tagGroupCombo.getText(), stime, etime, null);
 			}
 		});
 		
@@ -295,24 +302,26 @@ public class TagCountView extends ViewPart {
 			public void propertyChange(PropertyChangeEvent evt) {
 				Object o = evt.getNewValue();
 				if (o != null && o instanceof Range) {
-					rangeX1 = ((Range) o).getLower();
-					rangeX2 = ((Range) o).getUpper();
-					if (rangeX1 > rangeX2) {
-						double temp = rangeX2;
-						rangeX2 = rangeX1;
-						rangeX1 = temp;
+					double x1 = ((Range) o).getLower();
+					double x2 = ((Range) o).getUpper();
+					if (Math.abs(x1-x2) < DateUtil.MILLIS_PER_FIVE_MINUTE){
+						return;
 					}
-					if (rangeX2 - rangeX1 < DateUtil.MILLIS_PER_MINUTE) {
-						rangeX2 = rangeX1 + DateUtil.MILLIS_PER_MINUTE;
-						totalGraph.primaryXAxis.setRange(rangeX1, rangeX2);
+					if (x1 < x2) {
+						rangeX1 = x1;
+						rangeX2 = x2;
+					} else {
+						rangeX1 = x2;
+						rangeX2 = x1;
 					}
+					totalGraph.primaryXAxis.setRange(rangeX1, rangeX2);
 					zoomMode = true;
 					totalCanvas.notifyListeners(SWT.Resize, new Event());
 					adjustYAxisRange(totalGraph, (CircularBufferDataProvider) totalTrace.getDataProvider());
 					updateTextDate();
 				}
 			}
-		}, true);
+		}, false);
 		
 		Composite cntGraphComp = new Composite(graphSash, SWT.BORDER);
 		cntGraphComp.setBackground(ColorUtil.getInstance().getColor(SWT.COLOR_WHITE));
@@ -340,6 +349,23 @@ public class TagCountView extends ViewPart {
 			}
 		});
 		
+		cntCanvas.addMouseListener(new MouseListener() {
+			
+			public void mouseUp(MouseEvent e) {
+			}
+			
+			public void mouseDown(MouseEvent e) {
+			}
+			
+			public void mouseDoubleClick(MouseEvent e) {
+				long stime = (long) cntGraph.primaryXAxis.getPositionValue(e.x, true);
+				if (stime < rangeX1 || stime > rangeX2) return;
+				stime = stime / DateUtil.MILLIS_PER_MINUTE * DateUtil.MILLIS_PER_MINUTE;
+				long etime = stime + DateUtil.MILLIS_PER_MINUTE - 1;
+				loadInitData(tagGroupCombo.getText(), stime, etime, makeFilterMv());
+			}
+		});
+		
 		cntGraph = new XYGraph();
 		cntGraph.setShowLegend(true);
 		cntGraph.setShowTitle(false);
@@ -359,24 +385,26 @@ public class TagCountView extends ViewPart {
 			public void propertyChange(PropertyChangeEvent evt) {
 				Object o = evt.getNewValue();
 				if (o != null && o instanceof Range) {
-					rangeX1 = ((Range) o).getLower();
-					rangeX2 = ((Range) o).getUpper();
-					if (rangeX1 > rangeX2) {
-						double temp = rangeX2;
-						rangeX2 = rangeX1;
-						rangeX1 = temp;
+					double x1 = ((Range) o).getLower();
+					double x2 = ((Range) o).getUpper();
+					if (Math.abs(x1-x2) < DateUtil.MILLIS_PER_FIVE_MINUTE){
+						return;
 					}
-					if (rangeX2 - rangeX1 < DateUtil.MILLIS_PER_MINUTE) {
-						rangeX2 = rangeX1 + DateUtil.MILLIS_PER_MINUTE;
-						cntGraph.primaryXAxis.setRange(rangeX1, rangeX2);
+					if (x1 < x2) {
+						rangeX1 = x1;
+						rangeX2 = x2;
+					} else {
+						rangeX1 = x2;
+						rangeX2 = x1;
 					}
+					cntGraph.primaryXAxis.setRange(rangeX1, rangeX2);
 					zoomMode = true;
 					cntCanvas.notifyListeners(SWT.Resize, new Event());
 					adjustYAxisRange(cntGraph, cntTraceMap.values());
 					updateTextDate();
 				}
 			}
-		}, true);
+		}, false);
 		
 		graphSash.setWeights(new int[] {1, 1});
 		graphSash.setMaximizedControl(totalComp);
@@ -401,7 +429,7 @@ public class TagCountView extends ViewPart {
 				if (o instanceof TagCount) {
 					TagCount tc = (TagCount) o;
 					if (tc.tagName == null && tc.count == 0) {
-						loadTagValues(tagGroupCombo.getText(), tc.name);
+						loadTagValues(tagGroupCombo.getText(), tc.value);
 					} else {
 						TreeItem[] items = tagNameTree.getSelection();
 						if (items != null && items.length >0) {
@@ -427,9 +455,9 @@ public class TagCountView extends ViewPart {
 					TagCount tc = (TagCount) event.getElement();
 					if (StringUtil.isNotEmpty(tc.tagName)) {
 						if (event.getChecked()) {
-							loadTagCount(tagGroupCombo.getText(), tc.tagName, tc.name);
+							loadTagCount(tagGroupCombo.getText(), tc.tagName, tc.value);
 						} else {
-							removeTagCount(tc.name);
+							removeTagCount(tc.value);
 						}
 					}
 				}
@@ -453,10 +481,18 @@ public class TagCountView extends ViewPart {
 		rightTablecomp.setLayout(new GridLayout(1,true));
 		Composite tableInfoComp = new Composite(rightTablecomp, SWT.NONE);
 		tableInfoComp.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
-		tableInfoComp.setLayout(new GridLayout(3, false));
+		tableInfoComp.setLayout(new GridLayout(4, false));
 
+		rangeLbl = new Label(tableInfoComp, SWT.CENTER);
+		gd = new GridData(SWT.FILL, SWT.CENTER, true, false);
+		rangeLbl.setLayoutData(gd);
+		FontData fontData = rangeLbl.getFont().getFontData()[0];
+		Font font = new Font(getViewSite().getShell().getDisplay(), new FontData(fontData.getName(), fontData
+		    .getHeight(), SWT.BOLD));
+		rangeLbl.setFont(font);
+		
 		dataRangeLbl = new Label(tableInfoComp, SWT.RIGHT);
-		gd = new GridData(SWT.RIGHT, SWT.CENTER, true, false);
+		gd = new GridData(SWT.RIGHT, SWT.CENTER, false, false);
 		gd.widthHint = 100;
 		dataRangeLbl.setLayoutData(gd);
 		
@@ -468,7 +504,11 @@ public class TagCountView extends ViewPart {
 		leftBtn.setEnabled(false);
 		leftBtn.addSelectionListener(new SelectionAdapter() {
 			public void widgetSelected(SelectionEvent e) {
-				loadLeftData(tagGroupCombo.getText());
+				if (graphSash.getMaximizedControl() == cntCanvas.getParent()) {
+					loadLeftData(tagGroupCombo.getText(), makeFilterMv());
+				} else {
+					loadLeftData(tagGroupCombo.getText(), null);
+				}
 			}
 		});
 		
@@ -480,13 +520,18 @@ public class TagCountView extends ViewPart {
 		rightBtn.setEnabled(false);
 		rightBtn.addSelectionListener(new SelectionAdapter() {
 			public void widgetSelected(SelectionEvent e) {
-				loadRightData(tagGroupCombo.getText());
+				if (graphSash.getMaximizedControl() == cntCanvas.getParent()) {
+					loadRightData(tagGroupCombo.getText(), makeFilterMv());
+				} else {
+					loadRightData(tagGroupCombo.getText(), null);
+				}
 			}
 		});
 		
 		dataTableSash = new SashForm(rightTablecomp, SWT.HORIZONTAL);
 		dataTableSash.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 		serviceTable = new ServiceTableComposite(dataTableSash, SWT.NONE);
+		alertTable = new AlertTableComposite(dataTableSash, SWT.NONE);
 		
 		tableSash.setWeights(new int[] {1, 5});
 		tableSash.setMaximizedControl(null);
@@ -593,7 +638,7 @@ public class TagCountView extends ViewPart {
 		if ("service".equals(tagGroup)) {
 			dataTableSash.setMaximizedControl(serviceTable);
 		} else if ("alert".equals(tagGroup)) {
-			
+			dataTableSash.setMaximizedControl(alertTable);
 		}
 	}
 	
@@ -707,7 +752,7 @@ public class TagCountView extends ViewPart {
 							nameTree.clear();
 							for (int i = 0; i < list.size(); i++) {
 								TagCount tag = new TagCount();
-								tag.name = list.get(i);
+								tag.value = list.get(i);
 								nameTree.put(list.get(i), tag);
 							}
 							treeViewer.refresh();
@@ -763,13 +808,13 @@ public class TagCountView extends ViewPart {
 						if (parentTag == null) return;
 						for (int i = 0; i < nameList.size(); i++) {
 							TagCount child = new TagCount();
-							child.tagName = parentTag.name;
-							child.name = nameList.get(i);
+							child.tagName = parentTag.value;
+							child.value = nameList.get(i);
 							child.count = cntList.get(i);
 							parentTag.addChild(child);
 						}
 						parentTag.count = totalCount.value;
-						parentTag.name += " (" + valueSize.value + ")";
+						parentTag.value += " (" + valueSize.value + ")";
 						treeViewer.refresh();
 						treeViewer.setExpandedElements(new TagCount[] {parentTag});
 					}
@@ -900,6 +945,24 @@ public class TagCountView extends ViewPart {
 		return trace;
 	}
 	
+	private MapValue makeFilterMv() {
+		MapValue filterMv = new MapValue();
+		Object[] objects = treeViewer.getCheckedElements();
+		for (Object o : objects) {
+			if (o instanceof TagCount) {
+				TagCount tc = (TagCount) o;
+				if (tc.tagName == null) continue;
+				String tagName = tc.tagName;
+				ListValue lv = filterMv.getList(tagName);
+				if (lv == null) {
+					lv = filterMv.newList(tagName);
+				}
+				lv.add(TagCountUtil.convertTagToValue(tagName, tc.value));
+			}
+		}
+		return filterMv;
+	}
+	
 	int lastIndex;
 	int lastSize;
 	long firstTime;
@@ -907,7 +970,7 @@ public class TagCountView extends ViewPart {
 	long firstTxid;
 	long lastTxid;
 	
-	private void loadInitData(final String tagGroup, final long stime, final long etime) {
+	private void loadInitData(final String tagGroup, final long stime, final long etime, final MapValue filterMv) {
 		lastIndex = 0;
 		lastSize = 0;
 		firstTime = 0;
@@ -918,11 +981,12 @@ public class TagCountView extends ViewPart {
 		rightBtn.setEnabled(false);
 		ExUtil.asyncRun(new Runnable() {
 			public void run() {
-				List<Pack> list = loadData(tagGroup, stime, etime, false, null);
+				List<Pack> list = loadData(tagGroup, stime, etime, false, null, filterMv);
 				final ArrayList<Pack> result = new ArrayList<Pack>(list);
 				lastIndex = lastSize = result.size();
 				ExUtil.exec(dataTableSash, new Runnable() {
 					public void run() {
+							rangeLbl.setText(DateUtil.format(stime, "HH:mm") + " ~ " + DateUtil.format(stime + DateUtil.MILLIS_PER_MINUTE, "HH:mm"));
 							dataRangeLbl.setText(lastSize > 0 ? "1 ~ " + lastIndex : "0");
 							if (lastIndex == LIMIT_PER_PAGE) {
 								rightBtn.setEnabled(true);
@@ -939,6 +1003,16 @@ public class TagCountView extends ViewPart {
 									lastTxid = xp.txid;
 								}
 								serviceTable.setInput(result, serverId, tagGroup);
+							} else if ("alert".equals(tagGroup)) {
+								if (lastSize > 0) {
+									Pack p = result.get(0);
+									AlertPack xp = (AlertPack) p;
+									firstTime = xp.time;
+									p = result.get(result.size()-1);
+									xp = (AlertPack) p;
+									lastTime = xp.time;
+								}
+								alertTable.setInput(result, serverId, tagGroup);
 							}
 					}
 				});
@@ -946,7 +1020,7 @@ public class TagCountView extends ViewPart {
 		});
 	}
 	
-	private void loadRightData(final String tagGroup) {
+	private void loadRightData(final String tagGroup, final MapValue filterMv) {
 		ExUtil.asyncRun(new Runnable() {
 			public void run() {
 				long stime = lastTime;
@@ -955,7 +1029,7 @@ public class TagCountView extends ViewPart {
 				if ("service".equals(tagGroup)) {
 					extra.put("txid", lastTxid);
 				}
-				List<Pack> list = loadData(tagGroup, stime, etime, false, extra);
+				List<Pack> list = loadData(tagGroup, stime, etime, false, extra, filterMv);
 				final ArrayList<Pack> result = new ArrayList<Pack>(list);
 				lastSize = result.size();
 				ExUtil.exec(dataTableSash, new Runnable() {
@@ -981,6 +1055,19 @@ public class TagCountView extends ViewPart {
 								firstTxid = lastTxid = 0;
 							}
 							serviceTable.setInput(result, serverId, tagGroup);
+						} else if ("alert".equals(tagGroup)) {
+							if (lastSize > 0) {
+								Pack p = result.get(0);
+								AlertPack xp = (AlertPack) p;
+								firstTime = xp.time;
+								p = result.get(result.size()-1);
+								xp = (AlertPack) p;
+								lastTime = xp.time;
+							} else {
+								firstTime = lastTime + 1;
+								firstTxid = lastTxid = 0;
+							}
+							alertTable.setInput(result, serverId, tagGroup);
 						}
 					}
 				});
@@ -988,7 +1075,7 @@ public class TagCountView extends ViewPart {
 		});		
 	}
 	
-	private void loadLeftData(final String tagGroup) {
+	private void loadLeftData(final String tagGroup, final MapValue filterMv) {
 		ExUtil.asyncRun(new Runnable() {
 			public void run() {
 				long etime = firstTime;
@@ -997,7 +1084,7 @@ public class TagCountView extends ViewPart {
 				if ("service".equals(tagGroup)) {
 					extra.put("txid", firstTxid);
 				}
-				List<Pack> list = loadData(tagGroup, stime, etime, true, extra);
+				List<Pack> list = loadData(tagGroup, stime, etime, true, extra, filterMv);
 				final ArrayList<Pack> revList = new ArrayList<Pack>();
 				for (Pack p : list) {
 					revList.add(0, p);
@@ -1021,6 +1108,14 @@ public class TagCountView extends ViewPart {
 							lastTime = xp.endTime;
 							lastTxid = xp.txid;
 							serviceTable.setInput(revList, serverId, tagGroup);
+						} else if ("service".equals(tagGroup)) {
+							Pack p = revList.get(0);
+							AlertPack xp = (AlertPack) p;
+							firstTime = xp.time;
+							p = revList.get(revList.size()-1);
+							xp = (AlertPack) p;
+							lastTime = xp.time;
+							alertTable.setInput(revList, serverId, tagGroup);
 						}
 					}
 				});
@@ -1028,7 +1123,7 @@ public class TagCountView extends ViewPart {
 		});	
 	}
 	
-	private List<Pack> loadData(String tagGroup, long stime, long etime, boolean reverse, MapPack extra) {
+	private List<Pack> loadData(String tagGroup, long stime, long etime, boolean reverse, MapPack extra, MapValue filterMv) {
 		List<Pack> list = null;
 		TcpProxy tcp = TcpProxy.getTcpProxy(serverId);
 		try {
@@ -1048,9 +1143,12 @@ public class TagCountView extends ViewPart {
 					param.put(key, v);
 				}
 			}
+			if (filterMv != null) {
+				param.put("filter", filterMv);
+			}
 			list = tcp.process(RequestCmd.TAGCNT_TAG_ACTUAL_DATA, param);
 		} catch (Exception e) {
-			
+			e.printStackTrace();
 		} finally {
 			TcpProxy.putTcpProxy(tcp);
 		}
@@ -1068,7 +1166,7 @@ public class TagCountView extends ViewPart {
 			case 0:
 				if (obj instanceof TagCount) {
 					TagCount a = (TagCount) obj;
-					return a.name;
+					return a.value;
 				}
 			case 1:
 				if (obj instanceof TagCount) {
