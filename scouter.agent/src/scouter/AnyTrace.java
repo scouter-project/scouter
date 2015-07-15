360b75e4e9d5377a0e2f422bdaa15977cebdf06e
SkyWorker <skyworker.kim@gmail.com>
2015-07-16 00:27:52 +0900

    update comments

--- scouter.agent/src/scouter/AnyTrace.java
+++ scouter.agent/src/scouter/AnyTrace.java
@@ -1,4 +1,4 @@
-/*
+/**
  *  Copyright 2015 Scouter Project.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); 
@@ -12,6 +12,8 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License. 
+ *  
+ *  @author skyworker
  */
 
 package scouter;
