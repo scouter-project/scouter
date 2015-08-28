7dee4d38d9d0e105cba93e6dacfa24c9e7654dac
KimEunsu <eunsu.bill23@gmail.com>
2015-08-28 14:57:25 +0900

    add system util

--- scouter.common/src/scouter/util/SystemUtil.java
+++ scouter.common/src/scouter/util/SystemUtil.java
@@ -29,10 +29,12 @@ public class SystemUtil {
 	public static final boolean IS_JAVA_1_5 = System.getProperty("java.specification.version").startsWith("1.5");
 	public static final boolean IS_JAVA_1_6 = System.getProperty("java.specification.version").startsWith("1.6");
 	public static final boolean IS_JAVA_1_7 = System.getProperty("java.specification.version").startsWith("1.7");
+	public static final boolean IS_JAVA_1_8 = System.getProperty("java.specification.version").startsWith("1.8");
 	public static final String JAVA_VERSION = System.getProperty("java.version");
 	public static final String JAVA_SPEC_VERSION = System.getProperty("java.specification.version");
 
 	public static final String JAVA_VENDOR = System.getProperty("java.vendor");
+	public static final boolean IS_JAVA_IBM = JAVA_VENDOR.startsWith("IBM");
 
 	public static final String OS_NAME = System.getProperty("os.name");
 	public static final boolean IS_AIX = OS_NAME.startsWith("AIX");
