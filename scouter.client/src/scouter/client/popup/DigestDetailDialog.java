2b4bab114bfcc67a3c649d1254ca8b7aafe3681f
KimEunsu <eunsu.bill23@gmail.com>
2015-09-04 14:40:46 +0900

    unless example query, disappear tab

--- scouter.client/src/scouter/client/popup/DigestDetailDialog.java
+++ scouter.client/src/scouter/client/popup/DigestDetailDialog.java
@@ -417,7 +417,7 @@ public class DigestDetailDialog {
 				} finally {
 					TcpProxy.putTcpProxy(tcp);
 				}
-				if (p != null) {
+				if (p != null && ((MapPack)p).size() > 0) {
 					final MapPack m = (MapPack) p;
 					ExUtil.exec(tabFolder, new Runnable() {
 						public void run() {
