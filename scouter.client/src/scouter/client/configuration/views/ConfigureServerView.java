6feb14a10921b998c4dc26b0df0f258f2c415de5
KimEunsu <eunsu.bill23@gmail.com>
2015-09-15 22:33:49 +0900

    improve server configure view UX

--- scouter.client/src/scouter/client/configuration/views/ConfigureServerView.java
+++ scouter.client/src/scouter/client/configuration/views/ConfigureServerView.java
@@ -17,11 +17,18 @@
 package scouter.client.configuration.views;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IStatusLineManager;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.layout.TableColumnLayout;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.ColumnWeightData;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.custom.StyledText;
@@ -31,11 +38,18 @@ import org.eclipse.swt.dnd.Transfer;
 import org.eclipse.swt.events.KeyAdapter;
 import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 
@@ -43,17 +57,17 @@ import scouter.client.Images;
 import scouter.client.net.TcpProxy;
 import scouter.client.server.Server;
 import scouter.client.server.ServerManager;
-import scouter.client.util.ChartUtil;
+import scouter.client.sorter.ColumnLabelSorter;
 import scouter.client.util.ColoringWord;
 import scouter.client.util.ConsoleProxy;
+import scouter.client.util.CustomLineStyleListener;
 import scouter.client.util.ExUtil;
 import scouter.client.util.ImageUtil;
-import scouter.client.util.CustomLineStyleListener;
-import scouter.client.util.UIUtil;
 import scouter.lang.pack.MapPack;
 import scouter.lang.value.ListValue;
 import scouter.net.RequestCmd;
 import scouter.util.CastUtil;
+import scouter.util.StringUtil;
 
 public class ConfigureServerView extends ViewPart {
 	public final static String ID = ConfigureServerView.class.getName();
@@ -65,7 +79,11 @@ public class ConfigureServerView extends ViewPart {
 	private int serverId;
 	
 	Composite listComp;
+	TableViewer viewer;
 	Table table;
+	Text searchTxt;
+	
+	TableColumnLayout tableColumnLayout;
 	
 	private Clipboard clipboard = new Clipboard(null);
 	
@@ -77,18 +95,43 @@ public class ConfigureServerView extends ViewPart {
 		sashForm.SASH_WIDTH = 1;
 		initialStyledText(sashForm);
 		listComp = new Composite(sashForm, SWT.NONE);
-		listComp.setLayout(ChartUtil.gridlayout(1));
-		table = new Table(listComp,SWT.MULTI | SWT.BORDER | SWT.WRAP | SWT.FULL_SELECTION | SWT.V_SCROLL | SWT.H_SCROLL);
-		table.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.FILL_VERTICAL));
+		listComp.setLayout(new GridLayout(1, true));
+		searchTxt = new Text(listComp, SWT.BORDER);
+		searchTxt.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+		searchTxt.setToolTipText("Search Key/Value");
+		searchTxt.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				String searchText = searchTxt.getText();
+				if (StringUtil.isEmpty(searchText)) {
+					viewer.setInput(configList);
+				} else {
+					searchText = searchText.toLowerCase();
+					List<ConfObject> tempList = new ArrayList<ConfObject>();
+					for (ConfObject data : configList) {
+						String name = data.key.toLowerCase();
+						String value = data.value.toLowerCase();
+						if (name.contains(searchText) || value.contains(searchText)) {
+							tempList.add(data);
+						}
+					}
+					viewer.setInput(tempList);
+				}
+			}
+		});
+		Composite tableComp = new Composite(listComp, SWT.NONE);
+		tableComp.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		tableColumnLayout = new TableColumnLayout();
+		tableComp.setLayout(tableColumnLayout);
+		viewer = new TableViewer(tableComp, SWT.MULTI | SWT.BORDER | SWT.WRAP | SWT.FULL_SELECTION | SWT.V_SCROLL | SWT.H_SCROLL);
+		createColumns();
+		table = viewer.getTable();
 		table.setHeaderVisible(true);
 		table.setLinesVisible(true);
-		UIUtil.create(table, SWT.LEFT, "Key", 3, 0, false, 200);
-		UIUtil.create(table, SWT.CENTER, "Value", 3, 1, false, 100);
-		UIUtil.create(table, SWT.CENTER, "Def.", 3, 2, false, 100);
-		
+		viewer.setContentProvider(new ArrayContentProvider());
+	    viewer.setComparator(new ColumnLabelSorter(viewer));
 		table.addKeyListener(new KeyAdapter() {
 			public void keyPressed(KeyEvent e) {
-				if(e.stateMask == SWT.CTRL){
+				if(e.stateMask == SWT.CTRL || e.stateMask == SWT.COMMAND){
 					if (e.keyCode == 'c' || e.keyCode == 'C') {
 						TableItem[] items = table.getSelection();
 						if (items == null || items.length < 1) {
@@ -130,7 +173,7 @@ public class ConfigureServerView extends ViewPart {
 			public void keyReleased(KeyEvent e) {
 			}
 			public void keyPressed(KeyEvent e) {
-				if(e.stateMask == SWT.CTRL){
+				if(e.stateMask == SWT.CTRL || e.stateMask == SWT.COMMAND){
 					if(e.keyCode == 's'){
 						saveConfigurations();
 					}else if(e.keyCode == 'a'){
@@ -210,6 +253,8 @@ public class ConfigureServerView extends ViewPart {
 		});
 	}
 	
+	ArrayList<ConfObject> configList = new ArrayList<ConfObject>();
+	
 	private void loadConfigList() {
 		ExUtil.asyncRun(new Runnable() {
 			public void run() {
@@ -228,14 +273,15 @@ public class ConfigureServerView extends ViewPart {
 					final ListValue defaultList = pack.getList("default");
 					ExUtil.exec(listComp, new Runnable() {
 						public void run() {
-							table.removeAll();
+							configList.clear();
 							for (int i = 0; i < keyList.size(); i++) {
-								TableItem item = new TableItem(table, SWT.NONE);
-								item.setText(new String[] {CastUtil.cString(keyList.get(i)), 
-										CastUtil.cString(valueList.get(i)),
-										CastUtil.cString(defaultList.get(i)) });
+								ConfObject obj = new ConfObject();
+								obj.key = CastUtil.cString(keyList.get(i));
+								obj.value = CastUtil.cString(valueList.get(i));
+								obj.def = CastUtil.cString(defaultList.get(i));
+								configList.add(obj);
 							}
-							listComp.layout();
+							viewer.setInput(configList);
 						}
 					});
 				}
@@ -247,5 +293,69 @@ public class ConfigureServerView extends ViewPart {
 		IStatusLineManager slManager= getViewSite().getActionBars().getStatusLineManager();
 		slManager.setMessage("CTRL + S : save configurations, CTRL + A : select all text");
 	}
+	
+	private void createColumns() {
+		for (ConfEnum column : ConfEnum.values()) {
+			TableViewerColumn c = createTableViewerColumn(column.getTitle(), column.getWidth(), column.getAlignment(), column.isNumber());
+			ColumnLabelProvider labelProvider = null;
+			switch (column) {
+			case KEY:
+				labelProvider = new ColumnLabelProvider() {
+					@Override
+					public String getText(Object element) {
+						if (element instanceof ConfObject) {
+							return ((ConfObject) element).key;
+						}
+						return null;
+					}
+				};
+				break;
+			case VALUE:
+				labelProvider = new ColumnLabelProvider() {
+					@Override
+					public String getText(Object element) {
+						if (element instanceof ConfObject) {
+							return ((ConfObject) element).value;
+						}
+						return null;
+					}
+				};
+				break;
+			case DEFAULT:
+				labelProvider = new ColumnLabelProvider() {
+					@Override
+					public String getText(Object element) {
+						if (element instanceof ConfObject) {
+							return ((ConfObject) element).def;
+						}
+						return null;
+					}
+				};
+				break;
+			}
+			if (labelProvider != null) {
+				c.setLabelProvider(labelProvider);
+			}
+		}
+	}
+	
+	private TableViewerColumn createTableViewerColumn(String title, int width, int alignment, final boolean isNumber) {
+		final TableViewerColumn viewerColumn = new TableViewerColumn(viewer, SWT.NONE);
+		final TableColumn column = viewerColumn.getColumn();
+		column.setText(title);
+		column.setAlignment(alignment);
+		tableColumnLayout.setColumnData(column, new ColumnWeightData(30, width, true));
+		column.setData("isNumber", isNumber);
+		column.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				ColumnLabelSorter sorter = (ColumnLabelSorter) viewer.getComparator();
+				TableColumn selectedColumn = (TableColumn) e.widget;
+				sorter.setColumn(selectedColumn);
+			}
+		});
+		return viewerColumn;
+	}
+	
+	
 
 }
