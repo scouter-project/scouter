eff3cfdf081aee66669865c5aa6db764352a0cfa
Gunhee Lee <gunlee01@gmail.com>
2015-09-20 21:40:57 +0900

    ajax call timing -> use GET ajax call timing -> include res header(scouter_txid)

--- scouter.enduser.script/script/src/timing/scouter-ajax-timing.js
+++ scouter.enduser.script/script/src/timing/scouter-ajax-timing.js
@@ -7,10 +7,12 @@
   "use strict";
 
   var _p = window.scouter || {};
-  var DEFAULT_END_POINT = "/_scouter_browser.jsp";
+  var DEFAULT_END_POINT = '/_scouter_browser.jsp';
+  var DEFAULT_GXID_HEADER = 'scouter_gxid';
 
   _p.endPoint = _p.endPoint || DEFAULT_END_POINT;
   _p.debug = _p.debug || false;
+  _p.gxid_header = _p.gxid_header || DEFAULT_GXID_HEADER;
 
   var stats = [];
   var timeoutId = null;
@@ -31,23 +33,28 @@
 
     function onReadyStateChange() {
       if(self.readyState == 4 /* complete */) {
+        var resGxid = self.getResponseHeader(_p.gxid_header);
+
         var time = new Date() - start;
         stats.push({
           url: url,
-          duration: time
+          duration: time,
+          gxid: resGxid
         });
 
         if(!timeoutId) {
           timeoutId = window.setTimeout(function() {
+            var queryString = JSON.stringify({stats:stats}, undefined, 0);
             var xhr = new XHR();
             xhr.noIntercept = true;
-            xhr.open("POST", _p.endPoint + '?x=ajax', true);
-            xhr.setRequestHeader("Content-type","application/json");
-            xhr.send(JSON.stringify({ stats: stats } ));
+            var fullQuery = _p.endPoint + '?p=ax&q=' + encodeURIComponent(queryString);
+            console.log('fullQuery = ' + fullQuery);
+            xhr.open("GET", fullQuery, true);
+            xhr.send();
 
             timeoutId = null;
             stats = [];
-          }, 2000);
+          }, 1000);
         }
       }
 
