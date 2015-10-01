03e42825ef1e7c4f1a8fd51510dad17c2feb66cf
Gunhee Lee <gunlee01@gmail.com>
2015-10-01 20:05:42 +0900

    modify copyright comments

--- scouter.agent.host/src/scouter/agent/Logger.java
+++ scouter.agent.host/src/scouter/agent/Logger.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2015 LG CNS.
+ *  Copyright 2015 the original author or authors.
  *  
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
