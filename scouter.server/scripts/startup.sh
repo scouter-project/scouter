82c383cb4b493dc721245444c4368d26cfccd9ea
Gunhee Lee <gunlee01@gmail.com>
2015-11-08 13:23:40 +0900

    change server start script -> start as nohup & background mode

--- scouter.server/scripts/startup.sh
+++ scouter.server/scripts/startup.sh
@@ -1,3 +1,5 @@
-mkdir logs
-cp nohup.out > ./logs/nohup.(date '+%Y%m%d%H%M%S').out
+mkdir logs > /dev/null 2>&1
+cp nohup.out ./logs/nohup.$(date '+%Y%m%d%H%M%S').out > /dev/null 2>&1
 nohup java -Xmx512m -classpath ./boot.jar scouter.boot.Boot ./lib > nohup.out &
+echo "Scouter server launching..."
+echo "See the nohup.out."
