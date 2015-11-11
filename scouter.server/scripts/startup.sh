1f6046b4b36d71800d5687f08b56e4a5c0b513e7
kukgini <kukgini@gmail.com>
2015-11-11 11:50:05 +0900

    실행스크립트가 프로그램이 설치된 절대경로를 기반으로 동작하도록 개선; 디렉터리명에 공백이 포함되어도 오류가 나지 않도록 개선

--- scouter.server/scripts/startup.sh
+++ scouter.server/scripts/startup.sh
@@ -2,7 +2,7 @@
 
 . $(dirname $0)/env.sh
 
-mkdir -p $TUNAHOME/logs > /dev/null 2>&1
+mkdir -p "$TUNAHOME/logs" > /dev/null 2>&1
 cp nohup.out "$TUNAHOME/logs/nohup.$(date '+%Y%m%d%H%M%S').out" > /dev/null 2>&1
 nohup java $JAVAOPTS -classpath "$TUNAHOME/boot.jar" scouter.boot.Boot "$TUNAHOME/lib" > nohup.out &
 
