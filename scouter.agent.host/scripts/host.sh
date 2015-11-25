a5bca5ac291424836b0de84543b8936c2e6a3545
kukgini <kukgini@gmail.com>
2015-11-25 17:11:14 +0900

    improve start/stop script can be excuted outside installed directory

--- scouter.agent.host/scripts/host.sh
+++ scouter.agent.host/scripts/host.sh
@@ -1,1 +1,12 @@
-java  -classpath ./scouter.host.jar scouter.boot.Boot ./lib
+#!/usr/bin/env bash
+
+READLINK="`dirname $0`/readlink.sh"
+AGNTHOME="`dirname $($READLINK $0/..)`"
+
+mkdir $AGNTHOME/logs > /dev/null 2>&1
+cp $AGNTHOME/nohup.out $AGNTHOME/logs/nohup.$(date '+%Y%m%d%H%M%S').out > /dev/null 2>&1
+
+nohup java -classpath $AGNTHOME/scouter.host.jar scouter.boot.Boot $AGNTHOME/lib > $AGNTHOME/nohup.out &
+
+echo "Scouter host agent launching..."
+echo "See the nohup.out."
--- scouter.agent.host/scripts/stop.sh
+++ scouter.agent.host/scripts/stop.sh
@@ -1,1 +1,6 @@
-rm -f *.scouter
+#!/usr/bin/env bash
+
+READLINK="`dirname $0`/readlink.sh"
+AGNTHOME="`dirname $($READLINK $0/..)`"
+
+rm -f $AGNTHOME/*.scouter
