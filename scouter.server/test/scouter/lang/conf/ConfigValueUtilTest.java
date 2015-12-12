39aa23a396d7920c6dbce6d49159eb427efa00c3
Gunhee Lee <gunlee01@gmail.com>
2015-12-12 09:37:23 +0900

    add test

---
+++ scouter.server/test/scouter/lang/conf/ConfigValueUtilTest.java
@@ -0,0 +1,30 @@
+package scouter.lang.conf;
+
+import org.junit.Test;
+
+/**
+ * Created by gunlee on 2015. 12. 11..
+ */
+public class ConfigValueUtilTest {
+
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void testReplaceSysProp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetConfigDefault() throws Exception {
+
+    }
+
+    @Test
+    public void testToValue() throws Exception {
+
+    }
+}
