be5007f5198848cbf009da7472e662d311811938
KimEunsu <eunsu.bill23@gmail.com>
2016-05-13 17:36:12 +0900

    add option for excluding hooking method set

--- scouter.agent.java/src/scouter/agent/Configure.java
+++ scouter.agent.java/src/scouter/agent/Configure.java
@@ -168,6 +168,7 @@ public class Configure extends Thread {
 	public String hook_method_patterns = "";
 	public String hook_method_ignore_prefixes = "get,set";
 	public String hook_method_ignore_classes = "";
+	public String hook_method_exclude_patterns = "";
 	private StringSet _hook_method_ignore_classes = new StringSet();
 	public boolean hook_method_access_public_enabled = true;
 	public boolean hook_method_access_private_enabled = false;
@@ -376,6 +377,7 @@ public class Configure extends Thread {
 		this.profile_connection_open_enabled = getBoolean("profile_connection_open_enabled", true);
 		this._summary_connection_leak_fullstack_enabled = getBoolean("_summary_connection_leak_fullstack_enabled", false);
 		this.hook_method_patterns = getValue("hook_method_patterns", "");
+		this.hook_method_exclude_patterns = getValue("hook_method_exclude_patterns", "");
 		this.hook_method_access_public_enabled = getBoolean("hook_method_access_public_enabled", true);
 		this.hook_method_access_protected_enabled = getBoolean("hook_method_access_protected_enabled", false);
 		this.hook_method_access_private_enabled = getBoolean("hook_method_access_private_enabled", false);
