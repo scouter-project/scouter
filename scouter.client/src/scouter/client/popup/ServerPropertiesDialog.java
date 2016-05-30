509308f3787a33ee620c6db5f7040768d16ca6c6
KimEunsu <eunsu.bill23@gmail.com>
2016-05-30 16:44:15 +0900

    add properties

--- scouter.client/src/scouter/client/popup/ServerPropertiesDialog.java
+++ scouter.client/src/scouter/client/popup/ServerPropertiesDialog.java
@@ -78,6 +78,8 @@ public class ServerPropertiesDialog {
 		propertyList.add(new PropertyData("user id", server.getUserId()));
 		propertyList.add(new PropertyData("group", server.getGroup()));
 		propertyList.add(new PropertyData("timezone", server.getTimezone()));
+		propertyList.add(new PropertyData("session", server.getSession()));
+		propertyList.add(new PropertyData("time delta(ms)", server.getDelta()));
 		propertyTableViewer.refresh();
 		dialog.pack();
 		dialog.open();
