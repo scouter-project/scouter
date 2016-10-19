1adb52c38f97e7582112823284a65433ba6965ae
bill23 <eunsu.bill23@gmail.com>
2016-10-19 10:02:08 +0900

    change open location login dialog at startup to support macos sierra

--- scouter.client/src/scouter/client/ApplicationWorkbenchWindowAdvisor.java
+++ scouter.client/src/scouter/client/ApplicationWorkbenchWindowAdvisor.java
@@ -17,13 +17,9 @@
  */
 package scouter.client;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.TimeZone;
 
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.jface.window.Window;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.WorkbenchException;
@@ -33,15 +29,7 @@ import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
 import scouter.Version;
-import scouter.client.net.LoginMgr;
-import scouter.client.net.LoginResult;
-import scouter.client.popup.LoginDialog2.ILoginDialog;
-import scouter.client.popup.LoginDialog2;
-import scouter.client.preferences.PreferenceConstants;
-import scouter.client.preferences.ServerPrefUtil;
 import scouter.client.remote.CheckMyJob;
-import scouter.client.server.Server;
-import scouter.client.server.ServerManager;
 import scouter.client.threads.AlertProxyThread;
 import scouter.client.threads.SessionObserver;
 
@@ -57,7 +45,7 @@ import scouter.client.threads.SessionObserver;
  * ApplicationWorkbenchWindowAdvisor.dispose()
  */
 
-public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor implements ILoginDialog{
+public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 	
 	private ApplicationActionBarAdvisor actionBarAdvisor;
 	Display display;
@@ -87,63 +75,6 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor im
 		//configurer.setShowFastViewBars(false);
 		configurer.setShowPerspectiveBar(true);
 		
-		display = Display.getCurrent();
-		if (display == null) {
-			display = Display.getDefault();
-		}
-
-		boolean autoLogined = false;
-		String[] serverList = ServerPrefUtil.getStoredServerList();
-		if (serverList != null && serverList.length > 0) {
-			String[] autoList = ServerPrefUtil.getStoredAutoLoginServerList();
-			HashSet<String> autoSet = new HashSet<String>();
-			if (autoList != null && autoList.length > 0) {
-				autoSet = new HashSet<String>(Arrays.asList(autoList));
-			}
-			String defaultSrv = ServerPrefUtil.getStoredDefaultServer();
-			ServerManager manager = ServerManager.getInstance();
-			for (String addr : serverList) {
-				String[] iport = addr.split(":");
-				if (iport == null || iport.length < 2) {
-					continue;
-				}
-				Server server = new Server(iport[0], iport[1]);
-				if (addr.equals(defaultSrv)) {
-					manager.setDefaultServer(server);
-				} else {
-					manager.addServer(server);
-				}
-				if (autoSet.contains(addr)) {
-					String accountInfo = ServerPrefUtil.getStoredAccountInfo(addr);
-					if (accountInfo != null) {
-						int index = accountInfo.indexOf(PreferenceConstants.P_SVR_DIVIDER);
-						if (index > -1) {
-							String id = accountInfo.substring(0, index);
-							String pwd = accountInfo.substring(index + 1, accountInfo.length());
-							LoginResult result = LoginMgr.silentLogin(server, id, pwd);
-							if (result.success) {
-								autoLogined = true;
-							}
-						}
-					}
-				}
-			}
-			if (autoLogined && autoSet.contains(defaultSrv) == false) {
-				Set<Integer> openSet = manager.getOpenServerList();
-				Integer[] array = openSet.toArray(new Integer[openSet.size()]);
-				Server server = manager.getServer(array[0]);
-				ServerPrefUtil.storeDefaultServer(server.getIp()+":"+server.getPort());
-				ServerManager.getInstance().setDefaultServer(server);
-			}
-		}
-		if (autoLogined == false) {
-			LoginDialog2 dialog = new LoginDialog2(display.getActiveShell(), this, LoginDialog2.TYPE_STARTUP, null);
-			if (dialog.open() == Window.OK) {
-				
-			} else {
-				System.exit(0);
-			}
-		}
 		configurer.setTitle("Version - "+Version.getClientFullVersion() + "(" + TimeZone.getDefault().getDisplayName() + ")");
 	}
 	
@@ -188,10 +119,4 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor im
 		AlertProxyThread.getInstance();
 	}
 
-	@Override
-	public void loginSuccess(String serverAddr, int serverId) {
-		Server server = ServerManager.getInstance().getServer(serverId);
-		ServerPrefUtil.storeDefaultServer(server.getIp()+":"+server.getPort());
-		ServerManager.getInstance().setDefaultServer(server);
-	}
 }
