1b475ee05b2ceb08a0165be5f6bd460762bf7a58
Gunhee Lee <gunlee01@gmail.com>
2017-06-07 23:29:06 +0900

    add help page link on XLog view.

---
+++ scouter.client/src/scouter/client/constants/HelpConstants.java
@@ -0,0 +1,8 @@
+package scouter.client.constants;
+
+/**
+ * @author Gun Lee (gunlee01@gmail.com) on 2017. 6. 7.
+ */
+public class HelpConstants {
+    public static final String HELP_URL_XLOG_VIEW = "https://github.com/scouter-project/scouter/blob/master/scouter.document/index.md";
+}
