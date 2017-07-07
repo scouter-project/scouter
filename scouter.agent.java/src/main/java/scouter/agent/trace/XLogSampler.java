c66decc18533fd569fc3d91c6128dc3941f2321f
Gun Lee <gunlee01@gmail.com>
2017-07-07 10:07:11 +0900

    xlog sampling feature about specific service patterns

--- scouter.agent.java/src/main/java/scouter/agent/trace/XLogSampler.java
+++ scouter.agent.java/src/main/java/scouter/agent/trace/XLogSampler.java
@@ -11,6 +11,18 @@ import scouter.util.StringUtil;
  */
 public class XLogSampler {
 	private static Configure conf = Configure.getInstance();
+	private static StrMatch discardPatternMatch = new StrMatch(conf.xlog_discard_service_patterns);
+	private static StrMatch samplingPatternMatch = new StrMatch(conf.xlog_patterned_sampling_service_patterns);
+
+	public static void main(String[] args) {
+		String a = "100";
+		if((a="200").equals("100")) {
+			System.out.println(100);
+		} else {
+			System.out.println(a);
+		}
+
+	}
 
 	public static XLogDiscard evaluateXLogDiscard(int elapsed, String serviceName) {
 		XLogDiscard discardMode = XLogDiscard.DISCARD_NONE;
@@ -23,7 +35,28 @@ public class XLogSampler {
 			return XLogDiscard.DISCARD_ALL;
 		}
 
-		if(conf.xlog_sampling_enabled) {
+		boolean isSamplingServicePattern = false;
+		if (conf.xlog_patterned_sampling_enabled && (isSamplingServicePattern = isSamplingServicePattern(serviceName))) {
+			if (elapsed < conf.xlog_patterned_sampling_step1_ms) {
+				if (Math.abs(KeyGen.next() % 100) >= conf.xlog_patterned_sampling_step1_rate_pct) {
+					discardMode = conf.xlog_patterned_sampling_only_profile ? XLogDiscard.DISCARD_PROFILE : XLogDiscard.DISCARD_ALL;
+				}
+			} else if (elapsed < conf.xlog_patterned_sampling_step2_ms) {
+				if (Math.abs(KeyGen.next() % 100) >= conf.xlog_patterned_sampling_step2_rate_pct) {
+					discardMode = conf.xlog_patterned_sampling_only_profile ? XLogDiscard.DISCARD_PROFILE : XLogDiscard.DISCARD_ALL;
+				}
+			} else if (elapsed < conf.xlog_patterned_sampling_step3_ms) {
+				if (Math.abs(KeyGen.next() % 100) >= conf.xlog_patterned_sampling_step3_rate_pct) {
+					discardMode = conf.xlog_patterned_sampling_only_profile ? XLogDiscard.DISCARD_PROFILE : XLogDiscard.DISCARD_ALL;
+				}
+			} else {
+				if (Math.abs(KeyGen.next() % 100) >= conf.xlog_patterned_sampling_over_rate_pct) {
+					discardMode = conf.xlog_patterned_sampling_only_profile ? XLogDiscard.DISCARD_PROFILE : XLogDiscard.DISCARD_ALL;
+				}
+			}
+		}
+
+		if(!isSamplingServicePattern && conf.xlog_sampling_enabled) {
 			if(elapsed < conf.xlog_sampling_step1_ms) {
 				if(Math.abs(KeyGen.next()%100) >= conf.xlog_sampling_step1_rate_pct) {
 					discardMode = conf.xlog_sampling_only_profile ? XLogDiscard.DISCARD_PROFILE : XLogDiscard.DISCARD_ALL;
@@ -46,12 +79,21 @@ public class XLogSampler {
 	}
 
 	private static boolean isDiscardServicePattern(String serviceName) {
-		String patterns = conf.xlog_discard_service_patterns;
-		if(StringUtil.isEmpty(patterns)) {
+		if(StringUtil.isEmpty(conf.xlog_discard_service_patterns)) {
 			return false;
 		}
-		StrMatch match = new StrMatch(patterns);
-		if (match.include(serviceName)) {
+		if (discardPatternMatch.include(serviceName)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private static boolean isSamplingServicePattern(String serviceName) {
+		if(StringUtil.isEmpty(conf.xlog_patterned_sampling_service_patterns)) {
+			return false;
+		}
+		if (samplingPatternMatch.include(serviceName)) {
 			return true;
 		} else {
 			return false;
