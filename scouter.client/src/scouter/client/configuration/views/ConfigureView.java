fa9c3117ec869c9e1506071620db54bd3bdb4d83
Gun Lee <gunlee01@gmail.com>
2017-08-23 18:56:28 +0900

    bug fix about un-popup a configure window when double-click on windows system

--- scouter.client/src/scouter/client/configuration/views/ConfigureView.java
+++ scouter.client/src/scouter/client/configuration/views/ConfigureView.java
@@ -300,7 +300,7 @@ public class ConfigureView extends ViewPart {
 					int end = selectedY;
 					while(start > 0) {
 						char c = contents.charAt(--start);
-						if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_') {
+						if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_' || c == '-') {
 							//expand backward
 						} else {
 							start++;
@@ -309,7 +309,7 @@ public class ConfigureView extends ViewPart {
 					}
 					while(end < contents.length()) {
 						char c = contents.charAt(end++);
-						if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_') {
+						if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_' || c == '-') {
 							//expand ahead
 						} else {
 							end--;
@@ -319,6 +319,7 @@ public class ConfigureView extends ViewPart {
 					selectedX = start;
 					selectedY = end;
 					selectedText = contents.substring(start, end);
+					text.setSelection(start, end);
 
 					if (configKeyNames.contains(selectedText)) {
 						String fullText = text.getText();
@@ -343,11 +344,14 @@ public class ConfigureView extends ViewPart {
 							value = value.substring(startPos);
 						}
 
-						ConfigureItemDialog dialog = new ConfigureItemDialog(parent.getShell(), selectedText, value, displayName,
-								descMap.get(selectedText), valueTypeMap.get(selectedText), objHash == 0 ? true : false, objHash);
-						if (dialog.open() == Window.OK) {
-							setTheConfig(selectedText, dialog.getValue(), dialog.getApplyScope());
-						}
+						final String valuef = value;
+						ExUtil.exec(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell().getDisplay(), () -> {
+							ConfigureItemDialog dialog = new ConfigureItemDialog(parent.getShell(), selectedText, valuef, displayName,
+									descMap.get(selectedText), valueTypeMap.get(selectedText), objHash == 0 ? true : false, objHash);
+							if (dialog.open() == Window.OK) {
+								setTheConfig(selectedText, dialog.getValue(), dialog.getApplyScope());
+							}
+						});
 					}
 				}
 			}
@@ -356,6 +360,7 @@ public class ConfigureView extends ViewPart {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				if(e.x == e.y) return;
+				System.out.println("selected : " + e.x + ", " + e.y);
 				selectedText = text.getText(e.x, e.y-1);
 				selectedTime = System.currentTimeMillis();
 				selectedX = e.x;
