f1375be8b74842997c0430e356106018375989c6
circlee <circlee7@gmail.com>
2017-11-17 18:30:35 +0900

    Update JDBCPreparedStatementASM.java
    com/mysql/cj/jdbc/ServerPreparedStatement  제외

--- scouter.agent.java/src/main/java/scouter/agent/asm/JDBCPreparedStatementASM.java
+++ scouter.agent.java/src/main/java/scouter/agent/asm/JDBCPreparedStatementASM.java
@@ -69,7 +69,6 @@ public class JDBCPreparedStatementASM implements IASM, Opcodes {
 		target.add("Altibase/jdbc/driver/AltibasePreparedStatement");
 		
 		// MySql Connector/j 6.X
-		target.add("com/mysql/cj/jdbc/ServerPreparedStatement");
                 target.add("com/mysql/cj/jdbc/PreparedStatement");
 
         // @skyworker - MySQL ServerPreparedStatement는 특별히 필드를 추가하지 않음
