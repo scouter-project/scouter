8d0010634f9ac73e539e687a968f081b94d90ef1
Gunhee Lee <gunlee01@gmail.com>
2019-03-10 15:34:27 +0900

    [webapp] add some counter values on the interaction counter response. fix some bugs with null pointer.

--- scouter.webapp/src/main/java/scouterx/webapp/layer/controller/InteractionCounterController.java
+++ scouter.webapp/src/main/java/scouterx/webapp/layer/controller/InteractionCounterController.java
@@ -19,10 +19,18 @@
 package scouterx.webapp.layer.controller;
 
 import io.swagger.annotations.Api;
+import scouter.lang.counters.CounterEngine;
+import scouter.util.HashUtil;
+import scouterx.webapp.framework.client.model.AgentModelThread;
+import scouterx.webapp.framework.client.model.AgentObject;
+import scouterx.webapp.framework.client.server.Server;
 import scouterx.webapp.framework.client.server.ServerManager;
 import scouterx.webapp.framework.util.ZZ;
+import scouterx.webapp.layer.service.ActiveServiceService;
+import scouterx.webapp.layer.service.CounterService;
 import scouterx.webapp.layer.service.InteractionCounterService;
 import scouterx.webapp.model.InteractionCounterData;
+import scouterx.webapp.model.scouter.SCounter;
 import scouterx.webapp.view.CommonResultView;
 
 import javax.inject.Singleton;
@@ -37,7 +45,12 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author Gun Lee (gunlee01@gmail.com) on 2018. 8. 24.
@@ -47,13 +60,31 @@ import java.util.List;
 @Singleton
 @Produces(MediaType.APPLICATION_JSON)
 public class InteractionCounterController {
+    public static final String JAVAEE = "javaee";
     @Context
     HttpServletRequest servletRequest;
+    static Set<String> hostCounterSet = new HashSet<>();
+    static Set<String> javaCounterSet = new HashSet<>();
+
+    public static final String CPU = "Cpu";
+    public static final String _CPU = ":" + CPU;
+
+    public static final String ACTIVE_SPEED = "ActiveSpeed";
+    public static final String _ACTIVE_SPEED = ":" + ACTIVE_SPEED;
+
+    static {
+        hostCounterSet.add(CPU);
+        javaCounterSet.add(ACTIVE_SPEED);
+    }
 
     private final InteractionCounterService service;
+    private final CounterService counterService;
+    private final ActiveServiceService activeServiceService;
 
     public InteractionCounterController() {
         this.service = new InteractionCounterService();
+        this.counterService = new CounterService();
+        this.activeServiceService = new ActiveServiceService();
     }
 
     /**
@@ -90,9 +121,58 @@ public class InteractionCounterController {
             @QueryParam("objHashes") @Valid @NotNull final String objHashes,
             @QueryParam("serverId") final int serverId) {
 
-        List<InteractionCounterData> counterList = service.retrieveRealTimeByObjHashes(ZZ.splitParamAsIntegerSet(objHashes),
-                ServerManager.getInstance().getServerIfNullDefault(serverId));
+        Set<Integer> objHashSet = ZZ.splitParamAsIntegerSet(objHashes);
+        Server server = ServerManager.getInstance().getServerIfNullDefault(serverId);
+        List<InteractionCounterData> counterList = service.retrieveRealTimeByObjHashes(objHashSet, server);
 
+        Set<Integer> resultObjHashSet = new HashSet<>();
+        for (InteractionCounterData data : counterList) {
+            resultObjHashSet.add(data.fromObjHash);
+            resultObjHashSet.add(data.toObjHash);
+        }
+        Map<String, SCounter> hostCounters = findHostCounters(resultObjHashSet, server);
+        Map<String, SCounter> javaCounters = findJavaCounters(resultObjHashSet, server);
+        AgentModelThread agentModel = AgentModelThread.getInstance();
+        for (InteractionCounterData data : counterList) {
+            AgentObject fromParent = agentModel.getAgentObject(data.fromObjHash);
+            AgentObject toParent = agentModel.getAgentObject(data.toObjHash);
+
+            data.addFromObjCounter(hostCounters.get(fromParent == null ? null : fromParent.getParentHash() + _CPU));
+            data.addFromObjCounter(javaCounters.get(data.fromObjHash + _ACTIVE_SPEED));
+            data.addToObjCounter(hostCounters.get(toParent == null ? null : toParent.getParentHash() + _CPU));
+            data.addToObjCounter(javaCounters.get(data.toObjHash + _ACTIVE_SPEED));
+        }
         return CommonResultView.success(counterList);
+    }
+
+    private Map<String, SCounter> findHostCounters(Set<Integer> objHashSet, Server server) {
+        Map<Integer, AgentObject> agentObjectMap = AgentModelThread.getInstance().getAgentObjectMap();
+        Set<Integer> parentHashes = objHashSet.stream()
+                .map(objHash -> agentObjectMap.get(objHash))
+                .filter(Objects::nonNull)
+                .map(agent -> HashUtil.hash(agent.getParentName()))
+                .collect(Collectors.toSet());
+
+        List<SCounter> hostCounters = counterService
+                .retrieveRealTimeCountersByObjHashes(parentHashes, hostCounterSet, server);
+
+        return hostCounters.stream().collect(Collectors.toMap(o -> o.getObjHash() + ":" + o.getName(), o -> o));
+    }
+
+    private Map<String, SCounter> findJavaCounters(Set<Integer> objHashSet, Server server) {
+        CounterEngine counterEngine = server.getCounterEngine();
+        Map<Integer, AgentObject> agentObjectMap = AgentModelThread.getInstance().getAgentObjectMap();
+
+        Set<Integer> javaHashes = objHashSet.stream()
+                .map(objHash -> agentObjectMap.get(objHash))
+                .filter(Objects::nonNull)
+                .filter(agent -> JAVAEE.equals(counterEngine.getFamilyNameFromObjType(agent.getObjType())))
+                .map(agent -> agent.getObjHash())
+                .collect(Collectors.toSet());
+
+        List<SCounter> javaCounters = counterService
+                .retrieveRealTimeCountersByObjHashes(javaHashes, javaCounterSet, server);
+
+        return javaCounters.stream().collect(Collectors.toMap(o -> o.getObjHash() + ":" + o.getName(), o -> o));
     }
 }
