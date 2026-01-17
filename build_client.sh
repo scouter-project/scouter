8fa98cf69eb8b4ec2257522a8ee92369e0265733
Gun Lee <gunlee01@gmail.com>
2026-01-17 13:59:06 +0900

    [client] Improve build compatibility and UI customization Fix Eclipse 2024-03 XML parsing issues and add UI preference settings

--- build_client.sh
+++ build_client.sh
@@ -19,5 +19,7 @@ if [ -z "$MVN" ]; then
     echo maven not found.
 	exit 1
 else
+    # Increase XML entity size limit for Eclipse 2024-03 metadata
+    export MAVEN_OPTS="-Djdk.xml.maxGeneralEntitySizeLimit=0 -Djdk.xml.totalEntitySizeLimit=0"
     $MVN -Dtycho.debug.resolver=true -X -f ./scouter.client.build/pom.xml clean package
 fi
